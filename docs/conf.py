f7adc3ba5e9447ba5e97b3b0f61463adf7b0a938
Daniel Pfurner <dpfurner@tsn.at>
2023-02-15 16:57:00 +0100

    Add Documentation theme/extensions

--- docs/conf.py
+++ docs/conf.py
@@ -20,15 +20,14 @@ release = '0.1.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "numpydoc", "sphinx.ext.viewcode"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
