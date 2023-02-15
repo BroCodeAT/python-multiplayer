0b92377ed21c88adb4b94644fb96650eb14b1d02
Daniel Pfurner <dpfurner@tsn.at>
2023-02-15 16:26:13 +0100

    Documentation

--- src/py_mp/__init__.py
+++ src/py_mp/__init__.py
@@ -2,6 +2,8 @@
 The py_mp package is a simple python module for creating a client-server network
 application. It is designed to be used for games and other applications that
 require a client-server architecture.
+
+The package provides multiple ways to communicate between the client and server.
 """
 
 from .network import NetworkServer, NetworkClient, CommandClient, CommandServer
